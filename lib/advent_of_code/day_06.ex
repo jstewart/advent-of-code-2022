51da4ea74dd55ae9567083431f4db1735a7e5c37
Jason Stewart <jason@bendyworks.com>
2022-12-06 09:38:40 -0500

    Day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,33 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("", trim: true)
+    |> find_marker(0, [], 4)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split("", trim: true)
+    |> find_marker(0, [], 14)
+  end
+
+  defp find_marker([], cur_pos, _buffer, _msg_sz), do: cur_pos
+
+  defp find_marker([h | t], cur_pos, buffer, msg_sz) when length(buffer) < msg_sz do
+    find_marker(t, cur_pos + 1, buffer ++ [h], msg_sz)
+  end
+
+  defp find_marker([h | t], cur_pos, buffer, msg_sz) do
+    # adjust window
+    buffer =
+      buffer
+      |> Enum.drop(1)
+      |> Enum.concat([h])
+
+    if MapSet.new(buffer) |> MapSet.size() == msg_sz do
+      cur_pos + 1
+    else
+      find_marker(t, cur_pos + 1, buffer, msg_sz)
+    end
   end
 end
